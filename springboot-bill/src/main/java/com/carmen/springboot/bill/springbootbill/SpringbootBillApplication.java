7ef379661527ffad0e8914d600178af96732bd5a
CarmSC <carmencabrera2035@gmail.com>
2024-04-13 23:26:16 +0200

    new project bill

---
+++ springboot-bill/src/main/java/com/carmen/springboot/bill/springbootbill/SpringbootBillApplication.java
@@ -0,0 +1,13 @@
+package com.carmen.springboot.bill.springbootbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBillApplication.class, args);
+	}
+
+}
