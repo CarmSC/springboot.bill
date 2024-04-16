1a1c37168f93c03e31d207bf79edf57739a5b61a
CarmSC <carmencabrera2035@gmail.com>
2024-04-16 14:48:20 +0200

    update AppConfig y pom.xml

--- springboot-bill/src/main/java/com/carmen/springboot/bill/springbootbill/AppConfig.java
+++ springboot-bill/src/main/java/com/carmen/springboot/bill/springbootbill/AppConfig.java
@@ -11,14 +11,14 @@ import com.carmen.springboot.bill.springbootbill.models.ItemBill;
 import com.carmen.springboot.bill.springbootbill.models.Product;
 
 @Configuration
-@PropertySource("classpath=data.properties")
+@PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class AppConfig {
 
       @Bean
       List<ItemBill> itemInvoice(){
 
-            Product p1= new Product("Auriculares", 900);
-            Product p2= new Product("TV Smart", 1800);
+            Product p1 = new Product("Auriculares", 900);
+            Product p2 = new Product("TV Smart 32", 1800);
             return Arrays.asList(new ItemBill(p1,2),
                                  new ItemBill(p2,1));
 
